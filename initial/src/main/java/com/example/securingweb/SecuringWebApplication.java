43d36f68ad60d30a5fe215df7dd4bf02143afe55
HuyHoangTran <23020536@vnu.edu.vn>
2025-09-22 12:23:45 +0700

    Initial commit

--- initial/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ initial/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuringWebApplication {
+public class tSecuringWebApplication {
 
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(SecuringWebApplication.class, args);
